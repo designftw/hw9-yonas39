cd949e7e6912305568764264b48096fd3dfe03e7
yonas39 <124627215+yonas39@users.noreply.github.com>
2023-04-20 19:34:57 -0400

    before uploading a video

--- tracker/index.js
+++ tracker/index.js
@@ -7,34 +7,82 @@ let $ = (selector, container = document) => container.querySelector(selector);
 const backend = Backend.from(
   "https://github.com/designftw/hw9-yonas39/tree/master/tracker/data.json"
 );
+const loginButton = document.querySelector("#login_button");
+const logooutButton = document.querySelector("#logout_button");
+const saveButton = document.querySelector("#save_button");
 
-backend.addEventListener("mv-login", (evt) => {
-  loginButton.hidden = true;
-  logoutButton.hidden = false;
-  myUsername.textContent = backend.user.username;
+///////////////////////////////////////////////////////////////
+/////////////////////LogIN / LogOut Start//////////////////////
+///////////////////////////////////////////////////////////////
+
+loginButton.addEventListener("click", async () => {
+  await backend.login();
 });
-
-// Load data
-// let storedData = JSON.parse(data.innerHTML);
-
-// for (let entry of storedData) {
-//   addEntry(entry);
-// }
-
-save_button.addEventListener("click", (event) => {
-  let dataToSave = $$(".entry > form").map((form) => {
-    let data = new FormData(form);
-    return Object.fromEntries(data.entries());
-  });
-
-  data.innerHTML = JSON.stringify(dataToSave, null, "\t");
+logooutButton.addEventListener("click", () => {
+  backend.logout();
 });
+backend.addEventListener("mv-login", async () => {
+  document.getElementById("app").classList.add("logged-in");
+  // document.getElementById("user_avatar").src = backend.user.avatar;
+  document.getElementById("username").textContent = backend.user.username;
+  document.getElementById("logout_button").hidden = false;
+  document.getElementById("login_button").hidden = true;
+  // appFieldset.disabled = false;
+  const storeData = await backend.load();
+  if (storedData) {
+    for (let entry of storeData) {
+      addEntry(entry);
+    }
+  }
+});
+backend.addEventListener("mv-logout", () => {
+  document.getElementById("app").classList.remove("logged-in");
+  document.getElementById("login_button").hidden = false;
+  document.getElementById("logout_button").hidden = true;
+  document.getElementById("username").hidden = true;
+  // appFieldset.disabled = true;
+});
+///////////////////////////////////////////////////////////////
+/////////////////////LogIN / LogOut End////////////////////////
+///////////////////////////////////////////////////////////////
+
+// ############################################################
+// ##################### Modify save ##########################
+// ############################################################
+// save_button.addEventListener("click", (event) => {
+//   let dataToSave = $$(".entry > form").map((form) => {
+//     let data = new FormData(form);
+//     return Object.fromEntries(data.entries());
+//   });
+
+//   data.innerHTML = JSON.stringify(dataToSave, null, "\t");
+// });
+saveButton.addEventListener("click", async (e) => {
+  await backend.store(getData());
+});
+// ############################################################
+// ##################### Modify End ##########################
+// ############################################################
 
 add_entry_button.addEventListener("click", (event) => {
   // Set current date and time as default
   let currentISODate = new Date().toISOString().substring(0, 19); // drop ms and timezone
   addEntry({ datetime: currentISODate });
 });
+
+// ###############################################
+// ##################Get data start####################
+// ###############################################
+
+function getData() {
+  return $$(".entry > form").map((form) => {
+    let data = new FormData(form);
+    return Object.fromEntries(data.entries());
+  });
+}
+// ###############################################
+// ##################Get data end#####################
+// ###############################################
 
 function addEntry(data) {
   let entry = entry_template.content.cloneNode(true);
